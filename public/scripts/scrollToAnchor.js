258d3620e8a29ae0f6754e009a15501d04904c2e
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-25 06:09:42 +0000

    css animations for compose drawer

--- public/scripts/scrollToAnchor.js
+++ public/scripts/scrollToAnchor.js
@@ -1,15 +1,26 @@
 //smoothly animate scroll to anchor on chevron click
 $(document).ready(function() {
 // get .new-tweet location
-  const compose = $('.new-tweet')
-  const composeTop = compose.offset().top;
+  const $compose = $('.new-tweet')
+  const composeTop = $compose.offset().top;
 // on chevron click
   $('#chevron').on("click", function(event){
     event.preventDefault();
 
     // scroll to that location with an offset of 120 for the navbar
     $("html, body").animate( 
-      { scrollTop: composeTop - 120 }, 500); 
+      { scrollTop: composeTop - 120 }, 500)
+      .promise()
+      .done(function(){
+        const $form = $compose.find('form');
+        if ($form.css('display') === 'none'){
+          $form.slideDown();
+          $compose.find('textarea').focus();  
+        } else {
+          $form.slideUp();
+        }
+        
+      }) 
   })
 
 });

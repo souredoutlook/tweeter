e3c334fd6f7d73e9bfcbe92d2d1c5f97f3f37676
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-25 07:34:44 +0000

    add scroll behaviour to fixed footer

---
+++ public/scripts/fixedBehaviour.js
@@ -0,0 +1,32 @@
+// fix the $navbar when the yOffSet >= $navbar.offset.top
+$(document).ready(function() {
+  const $navBar = $('#navbar');
+  const navBarTop = $navBar.offset().top;
+
+  const hero = $('#heroBird');
+  const $spacerDiv = $('<div>');
+  $spacerDiv.addClass('content') 
+
+  // const $tweets = $('#tweets');
+  const $chevronUp = $('#chevronUp');
+  const $compose = $('.new-tweet')
+  
+  $(window).on('scroll', function() {
+
+    if (this.pageYOffset >= navBarTop && $navBar.hasClass('sticky') === false) {
+      $navBar.addClass('sticky');
+      $spacerDiv.insertAfter(hero); //add a spacer after hero so that the fixed header does not hide the user profile
+    } else if (this.pageYOffset < navBarTop) {
+      $navBar.removeClass('sticky');
+      $('.content').remove(); // remove the $spacerDiv spacer if the user scrolls all the back back to the hero
+    }
+
+    if(this.pageYOffset + 120 > $compose.offset().top && $chevronUp.css('display') === 'none') {
+      $chevronUp.show();
+    } else if (this.pageYOffset + 120 <= $compose.offset().top) {
+      $chevronUp.hide();
+    }
+
+  })
+  
+});
